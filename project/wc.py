04acb63e30ec08355120c3597443a4b77f9fcdc9
Sunigel <31823001+sunmns@users.noreply.github.com>
2017-09-27 11:28:54 +0200

    Add files via upload

---
+++ project/wc.py
@@ -0,0 +1,19 @@
+import sys
+
+lines=0
+tokens=0
+characters=0
+vowels=0
+syllables=0
+
+for c in sys.stdin.read():
+	if c in '''аэиоуеёйөүюя': 
+		vowels=vowels+1
+	if c==':
+		lines=lines+1
+	characters=characters+1
+	if c==' ':
+		tokens=tokens+1
+syllables=vowels/tokens
+
+print(lines, tokens, characters, vowels, syllables)

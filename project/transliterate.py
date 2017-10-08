e6d7ab449a0ca854ed61ddf8f7eaaa759ad7054d
Sunigel <31823001+sunmns@users.noreply.github.com>
2017-10-08 19:23:07 +0200

    Add files via upload

---
+++ project/transliterate.py
@@ -0,0 +1,18 @@
+import sys
+
+sent_id=1
+dict = str.maketrans("абвгдеёжзийклмноөпрстуүфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОӨПРСТУҮФХЦЧШЩЪЫЬЭЮЯ", "abvgdeëžziiklmnoöprstuüfhcčšŝiyieûâABVGDEЁŽZIIKLMNOÖPRSTUÜFHCČŠŜIYIEÛÂ")
+
+for c in sys.stdin.readlines():
+	number=1
+	c=c.replace('.', ' .')
+	c=c.replace(',', ' ,')
+	c=c.replace(':', ' :')
+	c=c.replace(';', ' ;')
+	c=c.split(' ')
+	print(sent_id)
+	for a in c:
+		print('%d\t%s\t_\t_\t_\t_\t_\t_\t_\ttranslit=%s'%(number,a,a.translate(dict)))
+		number=number+1
+	sent_id=sent_id+1
+	

d9021e7753130bf4dbb2afb7d8def344ce97be92
Sunigel <31823001+sunmns@users.noreply.github.com>
2017-12-13 16:14:28 +0300

    Add files via upload

---
+++ project/tokeniser.py
@@ -0,0 +1,21 @@
+import sys
+
+sent_id=1
+ 
+for c in sys.stdin.readlines():
+	number=1
+	c=c.strip()
+	c=c.replace('.', ' .')
+	c=c.replace(',', ' ,')
+	c=c.replace(':', ' :')
+	c=c.replace(';', ' ;')
+	c=c.split(' ')
+	print('\n')
+	print('#sent_id=%d'%(sent_id))
+	print('#text=',' '.join(c))
+	for a in c:
+		print('%d\t%s\t_\t_\t_\t_\t_\t_\t_\t_'%(number,a))
+		number=number+1
+	sent_id=sent_id+1
+        
+	
